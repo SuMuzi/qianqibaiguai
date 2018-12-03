4afe94328878ff74b59057060421ce1903f3eaab
SuMuzi <38148543+SuMuzi@users.noreply.github.com>
2018-12-03 21:46:58 +0800

    Delete Judge_psword.py

--- Judge_psword.py
+++
@@ -1,8 +0,0 @@
-from DBHelper.MysqldbHelper import mysqlhelper
-
-def IsPaaword(param):
-    sql="select password from user where name=%s"
-    db=mysqlhelper()
-    rs=db.select_psword(sql,param)
-   # print(rs)
-    return rs[0]
